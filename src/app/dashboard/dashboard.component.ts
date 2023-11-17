100bb4f40a41635e72b72679275182942ea60563
Andreea <andelenasamur@gmail.com>
2023-11-17 13:36:06 +0100

    control flow - @if

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
+import { SidemenuComponent } from '@shared/sidemenu/sidemenu.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, SidemenuComponent],
+  imports: [RouterModule,SidemenuComponent],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
